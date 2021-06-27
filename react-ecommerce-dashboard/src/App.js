950eee8d6fe3588e99b0afd6e147dd851d52edfe
pavel99pop <pavel99pop@gmail.com>
2021-06-27 23:01:50 +0600

    React, react-bootstrap, react-router-dom setup and inital commit

---
+++ react-ecommerce-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SH Fashion</h1>
+      <button>Simple Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
