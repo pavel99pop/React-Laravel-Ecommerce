9b253c442ad39f98b4650517f6ce0eb12f633d75
pavel99pop <pavel99pop@gmail.com>
2021-06-29 11:47:45 +0600

    Login form and function for frontend and using login api to login

--- react-ecommerce-frontend/src/components/Login.js
+++ react-ecommerce-frontend/src/components/Login.js
@@ -3,16 +3,52 @@ import { useHistory } from "react-router-dom";
 import Header from "./Header";
 
 const Login = () => {
-  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const history = useHistory();
   useEffect(() => {
     localStorage.getItem("user-info") && history.push("/add");
   }, []);
+
+  const login = async () => {
+    const creds = { email, password };
+    const res = await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(creds),
+    });
+    const data = await res.json();
+    localStorage.setItem("user-info", JSON.stringify(data));
+    history.push("/add");
+  };
 
   return (
     <div>
       <Header />
       <h1>This is the login page</h1>
+      <div className="col-sm-6 offset-sm-3">
+        <input
+          type="text"
+          placeholder="Email"
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={login} className="btn btn-primary">
+          Login
+        </button>
+      </div>
     </div>
   );
 };
