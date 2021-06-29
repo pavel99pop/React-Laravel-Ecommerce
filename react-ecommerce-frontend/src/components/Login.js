c1285ffdd17c8597962852b0ed599b2fba5752b8
pavel99pop <pavel99pop@gmail.com>
2021-06-29 13:42:58 +0600

    Products table, product controller and model, product api, creating products with api request with img path in backend storage

--- react-ecommerce-frontend/src/components/Login.js
+++ react-ecommerce-frontend/src/components/Login.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import Header from "./Header";
+// import { Alert } from "react-bootstrap";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [error, setError] = useState(false);
 
   const history = useHistory();
   useEffect(() => {
@@ -22,8 +24,12 @@ const Login = () => {
       body: JSON.stringify(creds),
     });
     const data = await res.json();
-    localStorage.setItem("user-info", JSON.stringify(data));
-    history.push("/add");
+    if (!data.error) {
+      // setError(() => false);
+      localStorage.setItem("user-info", JSON.stringify(data));
+      history.push("/add");
+    } else alert(data.error);
+    // else setError(() => data.error);
   };
 
   return (
@@ -31,6 +37,11 @@ const Login = () => {
       <Header />
       <h1>This is the login page</h1>
       <div className="col-sm-6 offset-sm-3">
+        {/* {{ error } && (
+          <>
+            <Alert variant="danger">{error}</Alert>
+          </>
+        )} */}
         <input
           type="text"
           placeholder="Email"
